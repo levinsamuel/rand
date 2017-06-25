74d3809530c57cdc33e4b121ff490919a82782b2
Samuel Levin <samuel.levin@nextgate.com>
2017-06-24 22:29:07 -0700

    perl brainfuck interpreter
     Committer: Samuel Levin <samuel.levin@nextgate.com>
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   perl/brainfuck.pl

---
+++ perl/brainfuck.pl
@@ -0,0 +1,157 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw(say);
+use lib '/home/samuel.levin/lib/';
+use Encode qw(decode encode find_encoding);
+use Switch;
+    
+my $file = shift;
+
+if (! defined $file) {
+    die "No file argument provided";
+}
+
+sub fromfile {
+
+    open (my $in, "<", $_[0]) or die "Could not open this file: $file";
+
+    my @script;
+    while (<$in>) {
+
+        # Remove non-script characters
+        s/[^+-<>.,\[\]]//g;
+        
+        push @script, $_;
+    }
+    return @script;
+}
+
+sub fromarg {
+
+    my @script = [];
+    $script[0] = $_[0];
+    
+    return @script;
+}
+
+my @lines;
+if (-f $file) {
+    @lines = fromfile $file;
+} else {
+    @lines = fromarg $file;
+}
+
+my @script;
+for my $line (@lines) {
+
+    foreach my $char (split //, $line) {
+        push @script, $char;
+    }
+}
+
+my @l;
+my @tape = (0) x 16;
+my $len = 16;
+my $slen = scalar @script;
+my $pos = 0;
+my @inarr;
+        
+for (my $i = 0; $i < $slen; $i++) {
+
+    my $char = $script[$i];
+    switch ($char) {
+    
+        case '<' {
+            
+            if ($pos == 0) {
+                my @news = (0) x 16;
+                unshift @tape, @news;
+                $pos = 15;
+                $len += 16;
+            } else {
+                
+                $pos--;
+            }
+            #say "Position: $pos";
+        }
+        case '>' {
+            
+            if ($pos +1 == $len) {
+                my @news = (0) x 16;
+                push @tape, @news;
+                
+            }   
+            $pos++;
+            #say "Position: $pos";
+            
+        }
+        case '.' {
+        
+            print chr $tape[$pos];
+            # for debuging remove chr
+            #print $tape[$pos];
+        }
+        case ',' {
+        
+            if (scalar @inarr == 0) {
+            
+                my $input = <STDIN>;
+                foreach my $i (split //, $input) {
+                    push @inarr, ord $i;
+                }
+            }
+            my $first = shift @inarr;
+            $tape[$pos] = $first;
+        }
+        case '+' {
+        
+            #print '+';
+            my $val = $tape[$pos];
+            $tape[$pos] = ($val + 1) % 256;
+        }
+        case '-' {
+        
+            #print '-';
+            my $val = $tape[$pos];
+            $tape[$pos] = ($val + 255) % 256;
+            #say "new val".$tape[$pos];
+            
+        }
+        case '[' {
+        
+            if ($tape[$pos] == 0) {
+            
+                my $here = $i;
+                while ($script[$i] ne ']') {
+                    $i++;
+                    die "Expected matching ']' for '[', script position $here" if ($i == $slen);
+                }
+                
+            } else {
+            
+                unshift @l, $i;
+            }
+            #print '[';
+        }
+        case ']' {
+            
+            my $v;
+            if ($tape[$pos] == 0) {
+                $v = shift @l;
+            } else {
+                $v = $l[0];
+                $i = $v if (defined $v);
+            }
+            die "found ']' missing opening '[' at position $i" if (! defined $v);
+        }
+    }
+    
+}
+
+say '';
+say 'Program finished';
+say "array size: ".scalar @tape;
+say "final position: $pos";
+
