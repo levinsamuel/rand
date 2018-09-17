b37dba0844025c1f46eda908dfa23989a280b2e7
Samuel Levin <levinsamuel000@gmail.com>
2018-09-17 18:30:37 +0100

    fix rudiment script

--- bash/randomrud.sh
+++ bash/randomrud.sh
@@ -3,8 +3,10 @@
 IFSBAK=$IFS
 IFS=$'\n'
 
+me=$(readlink -f $0 | xargs dirname)
+
 i=0
-for url in $(cat ~/stuff/rudies2.txt); do
+for url in $(cat $me/../other/rudies2.txt); do
 
     urls[$i]=$url
     i=$((i+1))
