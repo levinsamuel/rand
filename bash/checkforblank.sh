ddb424d467a10ac4b95f5c5684118a4e45abe5ce
Samuel Levin <levinsamuel000@gmail.com>
2019-03-12 15:14:18 -0700

    small index fix

--- bash/checkforblank.sh
+++ bash/checkforblank.sh
@@ -5,10 +5,10 @@ for file in `ls $1`; do
   echo "checking $file"
   hd=(`head -n 1 $file | tr -d '\n' | tr , ' '`)
   echo "fields: ${#hd[@]}"
-  for i in `seq 1 ${#hd[@]}`; do
+  for ((i=1; i<=${#hd[@]}; i+=1)); do
 
     blanks=`cut -f $i -d , $file | grep -v '.' | wc -l`
-    echo "field is ${hd[i]}, $blanks blanks"
+    echo "field is ${hd[i-1]}, $blanks blanks"
   done
 
 done
